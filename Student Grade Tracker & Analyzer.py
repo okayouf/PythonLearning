ac144a4a8f0cb5c4c71454f4a64ecb7038bc05ad
okayouf <okayouf@gmail.com>
2026-03-17 11:20:09 +0200

    Add files via upload

---
+++ Student Grade Tracker & Analyzer.py
@@ -0,0 +1,15 @@
+
+name = "Yael Cohen"
+scores = [92, 78, 88]
+average_score = round(sum(scores) / len(scores), 1)
+passed = True
+
+print(type(name))
+print(type(scores))
+print(type(average_score))
+print(type(passed))
+
+students = ["Yael Cohen","Noam Levy","Tamar Shapira","Ori Mizrahi","Dana Peretz","Eitan Katz"]
+names_scores = {"Yael Cohen" : 92 ,"Noam Levy" : 65,"Tamar Shapira" : 95 ,"Ori Mizrahi": 45 ,"Dana Peretz" : 83,"Eitan Katz" : 71}
+subjects = ("Math","English","Science")
+student_names = {"Yael Cohen","Noam Levy","Tamar Shapira","Ori Mizrahi","Dana Peretz","Eitan Katz"}
